5d9d6a0660c96fb46049010ca1f9bf9f54a9f846
Christopher Seidl <KeilChris@users.noreply.github.com>
2021-10-21 12:00:17 +0200

    Add files via upload

---
+++ RTE/_GPIO_INT/RTE_Components.h
@@ -0,0 +1,29 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GPIO_INT' 
+ * Target:  'GPIO_INT' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "M2351.h"
+
+/* Nuvoton::Device:StdDriver:CLK:0.00.001 */
+#define RTE_Drivers_CLK              /* Driver CLK */
+/* Nuvoton::Device:StdDriver:GPIO:0.00.001 */
+#define RTE_Drivers_GPIO              /* Driver GPIO */
+/* Nuvoton::Device:StdDriver:SYS:0.00.001 */
+#define RTE_Drivers_SYS              /* Driver SYS */
+/* Nuvoton::Device:StdDriver:UART:0.00.001 */
+#define RTE_Drivers_UART              /* Driver UART */
+
+
+#endif /* RTE_COMPONENTS_H */
